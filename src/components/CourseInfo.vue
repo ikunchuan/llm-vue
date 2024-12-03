9e1b8783ac61d75a53020870db367e4776a0ca83
weishenying <1724579910@>
2024-12-03 14:35:44 +0800

    course完整

--- src/components/CourseInfo.vue
+++ src/components/CourseInfo.vue
@@ -3,28 +3,40 @@
         <template #header>
             <div slot="header" class="card-header">{{ name }}</div><br>
             <div class="header-actions">
-                <el-input v-model="queryStr" style="width: 220px" placeholder="请输入课程名称" />&nbsp;
-                <el-button type="primary" round @click="queryInfo">查询</el-button>
-                <el-button class="button" type="success" round @click="openAddDialog">添加</el-button>
-                <el-button class="button" type="warning" round @click="multipleDelete">多选删除</el-button>
+                <!-- 查询字段选择：让用户选择查询的字段 -->
+                <el-select v-model="selectedField" placeholder="选择查询字段" style="width: 180px;">
+                    <el-option label="课程名称" value="courseName"></el-option>
+                    <el-option label="课程简介" value="courseDescription"></el-option>
+                    <el-option label="难度级别" value="courseDifficultyLevel"></el-option>
+                    <el-option label="课程评分" value="courseRating"></el-option>
+                </el-select>&nbsp;
+
+                <!-- 输入框：输入查询内容 -->
+                <el-input v-model="queryStr" style="width: 220px" placeholder="请输入查询内容" />&nbsp;
+
+                <!-- <el-input v-model="queryStr" style="width: 220px" placeholder="请输入课程名称" />&nbsp; -->
+                
+                <el-button type="primary"  @click="queryInfo">查询</el-button>
+                <el-button class="button" type="success"  @click="openAddDialog">添加</el-button>
+                <el-button class="button" type="warning"  @click="multipleDelete">多选删除</el-button>
             </div>
         </template>
 
         <el-table :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
             <el-table-column fixed type="selection" width="55" />
-            <el-table-column prop="categoryId" label="类别" width="120" />
-            <el-table-column prop="courseName" label="课程名称" width="200" />
-            <el-table-column prop="courseDescription" label="课程简介" width="500" />
-            <el-table-column prop="courseDifficultyLevel" label="难度级别" width="150" />
-            <el-table-column prop="courseRating" label="评分" width="150" />
+            <el-table-column prop="categoryId" label="类别" width="70" />
+            <el-table-column prop="courseName" label="课程名称" width="100" />
+            <el-table-column prop="courseDescription" label="课程简介" width="100" />
+            <el-table-column prop="courseDifficultyLevel" label="难度级别" width="100" />
+            <el-table-column prop="courseRating" label="评分" width="70" />
 
-            <el-table-column prop="updatedTime" label="更新时间" width="250">
+            <el-table-column prop="updatedTime" label="更新时间" width="100">
                 <template #default="{ row }">
                     <span>{{ formatDate(row.updatedTime) }}</span>
                 </template>
             </el-table-column>
 
-            <el-table-column prop="createdTime" label="创建时间" width="250" >
+            <el-table-column prop="createdTime" label="创建时间" width="100" >
                 <template #default="{ row }">
                     <span>{{ formatDate(row.createdTime) }}</span>
                 </template>
@@ -40,7 +52,7 @@
         </el-table>
 
         <!-- 分页 -->
-        <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[5, 10, 15, 20]"
+        <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[3, 5, 10, 20]"
             layout="total, sizes, prev, pager, next, jumper" :total="pageInfo.total" @size-change="handleSizeChange"
             @current-change="handleCurrentChange" />
     </el-card>
@@ -136,6 +148,7 @@ export default {
         return {
             name: '课程信息',
             queryStr: "",
+            selectedField: "",  // 选择的查询字段
 
             currentPage: 1,
             pageSize: 5,
@@ -180,6 +193,14 @@ export default {
                 });
         },
 
+        // // 添加或编辑按钮点击事件
+        // btnAddUpdate() {
+        //     if (this.btnName === '添加') {
+        //         this.addCourse();
+        //     } else if (this.btnName === '编辑') {
+        //         this.updateCourse();
+        //     }
+        // },
 
         openAddDialog() {
             this.title = '添加课程信息';
@@ -200,19 +221,45 @@ export default {
             });
         },
 
+
+        //打开详情页
         openDetailDialog(courseId) {
+            // 打开详情对话框时，发送请求获取数据
             this.$http.get(`/crs/v1/${courseId}`).then((response) => {
-                this.form = response.data;
-                this.dialogDetailVisible = true;
+                if (response.data) {
+                    this.form = response.data;  // 设置表单数据
+                    this.dialogDetailVisible = true;  // 打开详情对话框
+                } else {
+                    ElMessage({ message: '未找到对应的课程信息', type: "warning" });
+                }
+            }).catch(() => {
+                ElMessage({ message: '请求失败，请重试', type: "error" });
             });
         },
 
+        // queryInfo() {
+        //     if (this.queryStr.trim().length > 0) {
+        //         this.tableData = this.pageInfo.records.filter(item =>
+        //             item.courseName && item.courseName.match(this.queryStr.trim())
+        //         );
+        //     } else {
+        //         this.tableData = this.pageInfo.records;// 恢复原数据
+        //     }
+        // },
+
+        // 查询方法，根据选择的字段和输入的查询内容进行查询
         queryInfo() {
-            if (this.queryStr.trim().length > 0) {
-                this.tableData = this.pageInfo.records.filter(item =>
-                    item.courseName && item.courseName.match(this.queryStr.trim())
-                );
+            // 如果选择了查询字段和输入了查询条件
+            if (this.queryStr.trim().length > 0 && this.selectedField) {
+                this.tableData = this.pageInfo.records.filter(item => {
+                    // 根据选择的字段进行匹配
+                    if (item[this.selectedField] && item[this.selectedField].includes(this.queryStr.trim())) {
+                        return true;  // 匹配成功
+                    }
+                    return false;  // 不匹配
+                });
             } else {
+                // 如果没有选择字段或者没有输入查询内容，恢复原数据
                 this.tableData = this.pageInfo.records;
             }
         },
@@ -225,35 +272,75 @@ export default {
             }).then(() => {
                 this.$http.delete(`/crs/v1/${courseId}`).then((response) => {
                     if (response.data === 1) {
-                        ElMessage({ message: '删除成功', type: 'success' });
-                        this.getPageData(this.currentPage, this.pageSize);
+                        ElMessage({ message: '删除成功', type: "success" });
+                        this.getPageData(this.currentPage, this.pageSize); // 刷新数据
                     } else {
-                        ElMessage({ message: '删除失败', type: 'warning' });
+                        ElMessage({ message: '删除失败', type: "warning" });
                     }
+                }).catch(() => {
+                    ElMessage({ message: '请求失败，请重试', type: 'error' });
                 });
             }).catch(() => { });
         },
 
+        // // 批量删除
+        // multipleDelete() {
+        //     if (this.multipleSelection.length > 0) {
+        //         // 从选中的课程中提取课程 ID
+        //         const ids = this.multipleSelection.map(item => item.courseId); // 确保 item 中有 courseId
+
+        //         ElMessageBox.confirm('是否删除选中的所有数据?', '批量删除提示', {
+        //             confirmButtonText: '确定',
+        //             cancelButtonText: '取消',
+        //             type: "warning",
+        //         }).then(() => {
+        //             const ids = this.multipleSelection.map(item => item.questionId);
+        //             // ${ids.join(",")}
+        //             this.$http.delete(`/crs/v1`, { data: ids }).then((response) => {
+        //                 if (response.data > 0) {
+        //                     ElMessage({ message: '批量删除成功', type: "success" });
+        //                     this.getPageData(this.currentPage, this.pageSize); // 刷新数据
+        //                 } else {
+        //                     ElMessage({ message: '批量删除失败', type: "warning" });
+        //                 }
+        //             }).catch(() => {
+        //                 ElMessage({ message: '请求失败，请重试', type: "error" });
+        //             });
+        //         }).catch(() => { });
+        //     } else {
+        //         ElMessage({ message: '请选择要删除的记录', type: "warning" });
+        //     }
+        // },
+
         multipleDelete() {
+            // 确保有选中的课程
             if (this.multipleSelection.length > 0) {
+                // 从选中的课程中提取课程 ID
+                const ids = this.multipleSelection.map(item => item.courseId); // 确保 item 中有 courseId
+
+                // 调用接口删除课程
                 ElMessageBox.confirm('是否删除选中的所有数据?', '批量删除提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
-                    type: 'warning',
+                    type: "warning",
                 }).then(() => {
-                    this.$http.post('/course/v1/delete', this.multipleSelection).then((response) => {
-                        if (response.data === 1) {
-                            ElMessage({ message: '批量删除成功', type: 'success' });
-                            this.getPageData(this.currentPage, this.pageSize);
+                    // 发送 DELETE 请求，携带选中的课程 IDs
+                    this.$http.delete(`/crs/v1`, { data: ids }).then((response) => {
+                        if (response.data > 0) {
+                            ElMessage({ message: '批量删除成功', type: "success" });
+                            this.getPageData(this.currentPage, this.pageSize); // 刷新数据
                         } else {
-                            ElMessage({ message: '批量删除失败', type: 'warning' });
+                            ElMessage({ message: '批量删除失败', type: "warning" });
                         }
+                    }).catch(() => {
+                        ElMessage({ message: '请求失败，请重试', type: "error" });
                     });
                 }).catch(() => { });
             } else {
-                ElMessage({ message: '请选择要删除的记录', type: 'warning' });
+                ElMessage({ message: '请选择要删除的记录', type: "warning" });
             }
         },
+
 
         handleSelectionChange(val) {
             this.multipleSelection = val;
@@ -267,16 +354,44 @@ export default {
             }
         },
 
+        // updateCourse() {
+        //     this.$http.put(`/crs/v1/${this.form.courseId}`, this.form).then((response) => {
+        //         if (response.data === 1) {
+        //             ElMessage({ message: '更新成功', type: 'success' });
+        //             this.getPageData(this.currentPage, this.pageSize);
+        //             this.dialogFormVisible = false;
+        //         } else {
+        //             ElMessage({ message: '更新失败', type: 'error' });
+        //         }
+        //     })
+        // },
+
+        // 打开编辑对话框
+        openUpdateDialog(row) {
+            this.title = '编辑课程信息';
+            this.btnName = '编辑';
+            this.form = { ...row }; // 填充表单数据
+            this.dialogFormVisible = true;
+        },
+
+
+        // 编辑课程信息
         updateCourse() {
-            this.$http.put(`/crs/v1/${this.form.courseId}`, this.form).then((response) => {
-                if (response.data === 1) {
-                    ElMessage({ message: '更新成功', type: 'success' });
-                    this.getPageData(this.currentPage, this.pageSize);
-                    this.dialogFormVisible = false;
+            if (!this.form.categoryId || !this.form.courseName || !this.form.courseDescription || !this.form.courseDifficultyLevel) {
+                ElMessage({ message: '请填写完整的信息！', type: "warning" });
+                return;
+            }
+            this.$http.put(`/crs/v1`, this.form).then((response) => {
+                if (response.data == 1) {
+                    ElMessage({ message: '更新成功', type: "success" });
+                    this.getPageData(this.currentPage, this.pageSize); // 刷新数据
+                    this.dialogFormVisible = false; // 关闭对话框
                 } else {
-                    ElMessage({ message: '更新失败', type: 'error' });
+                    ElMessage({ message: '更新失败', type: "error" });
                 }
-            })
+            }).catch((err) => {
+                ElMessage({ message: '请求失败，请重试', type: "error" });
+            });
         },
 
         formatDate(value) {
